d068ac7eeae95461b84d34a5e1742a26ae205d15
jyotigunvare <128350085+jyotigunvare@users.noreply.github.com>
2023-03-31 13:25:03 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
-
+// This function clear all the values
+const a=document.getElementById("result");
+function clearScreen() {
+        a.value = "";
+    }
+     
+    // This function display values
+    function display(value) {
+        a.value += value;
+    }
+     
+    // This function evaluates the expression and returns result
+    function calculate() {
+        var p = a.value;
+        var q = eval(p);
+        a.value = q;
+    }    
+    function deleteData(){
+        a.value =a.value.substr(0,a.value.length-1);
+    }
